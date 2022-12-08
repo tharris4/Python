c19a0abe84044f9282a52861205255e4d9e92355
Thomas Harris <tharris4@gmail.com>
2022-12-08 08:27:00 -0500

    Fixed csvreader

--- WaitesDataParsing/runtime.py
+++ WaitesDataParsing/runtime.py
@@ -12,12 +12,12 @@ samples = []
 
 #open the waites csv file and create list of timestamps
 with open("ag1009.csv") as csvdata:
-    csv_reader = csv.reader(csvdata, delimiter=',')
+    csv_reader = csv.DictReader(csvdata)
     errCount = 0
     for row in csv_reader:
-        timeStamp = time.mktime(datetime.datetime.strptime(row[0], "%m/%d/%Y %H:%M").timetuple())
+        timeStamp = time.mktime(datetime.datetime.strptime(row['DateTime'], "%m/%d/%Y %H:%M").timetuple())
         try:
-            amplitude = float(row[1])
+            amplitude = float(row['Gearbox - DS'])
         except:
             errCount +=1
             amplitude = 0
@@ -26,10 +26,16 @@ with open("ag1009.csv") as csvdata:
 
 print("Found", errCount, "error" if errCount == 1 else "errors.")
 
-highCount = 0
+runTime = 0
+inRun = False
 
 for dataPoint in samples:
-    if dataPoint.amplitude > 0.01:
-        highCount +=1
+    if dataPoint.amplitude > 0.01 and not inRun:
+        startRun = dataPoint.timestamp
+        inRun = True
+    elif dataPoint.amplitude < 0.01 and inRun:
+        endRun = dataPoint.timestamp
+        runTime += (endRun - startRun)
+        inRun = False
 
-print(highCount)
+print(runTime/60/60, "Hours")
